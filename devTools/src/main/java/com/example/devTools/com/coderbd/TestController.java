effecda92f88f4004b30da63a51531275ef100a0
Sharif Hossain <mamundndc@gmail.com>
2019-06-23 18:58:12 +0600

    e

---
+++ devTools/src/main/java/com/example/devTools/com/coderbd/TestController.java
@@ -0,0 +1,18 @@
+package com.example.devTools.com.coderbd;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping(value = "/")
+    public String index(){
+        return "Hello Spring Rest Api";
+    }
+    @GetMapping(value = "/test")
+    public String indexTest(){
+        return "Hello Spring Rest Api";
+    }
+
+}
---
+++ springbeenlifevycleone/src/main/java/com/example/springbeenlifevycleone/SpringbeenlifevycleoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbeenlifevycleone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbeenlifevycleoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbeenlifevycleoneApplication.class, args);
+	}
+
+}
