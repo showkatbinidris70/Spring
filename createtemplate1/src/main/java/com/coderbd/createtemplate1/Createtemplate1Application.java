add7fbe2cb80b6248fa0e097c456498789a44b7c
Sharif Hossain <mamundndc@gmail.com>
2019-06-27 19:05:41 +0600

    l

---
+++ createtemplate1/src/main/java/com/coderbd/createtemplate1/Createtemplate1Application.java
@@ -0,0 +1,13 @@
+package com.coderbd.createtemplate1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Createtemplate1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Createtemplate1Application.class, args);
+    }
+
+}
---
+++ createtemplate1/src/test/java/com/coderbd/createtemplate1/Createtemplate1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.coderbd.createtemplate1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Createtemplate1ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
